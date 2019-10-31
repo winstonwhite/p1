fc9b205cabbaf4315cdf98ed063dd4a64b21163e
Winston R White <winston.white@my.liu.edu>
2019-10-31 11:58:42 -0400

    update2

---
+++ script.js
@@ -0,0 +1,16 @@
+// // let action = document.querySelector('img-wrapper')
+// let question = document.getElementsByClassName("img-wrapper1");
+
+// for (var i = 0; i < question.length ; i++)
+// {
+//     question.addEventListener("click", function () { Window.promt('Are you sure?'); }, false);
+// }
+
+document.getElementById("button").onclick = function(){
+    alert("Sorry Dude out of Stock")
+};
+
+document.getElementById("button2").onclick = function(){
+    alert("The Demogorgons invaded the site and snatched the items, in order to find them you may have to find a way to reach the upside down and collect the stolen items.")
+};
+
